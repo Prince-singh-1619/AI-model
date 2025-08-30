7dd4d43e415c40af9312a478ee3fbd2ea23c8b56
Prince Singh <Princesinghps1619@gmail.com>
2025-08-30 12:27:42 +0530

    fixed unused states

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig(( {command}) =>({
   plugins: [react(), tailwindcss(),],
-  base: command === 'build' ? '/AI-model}/' : '/',
+  base: command === 'build' ? '/AI-model/' : '/',
 }))
